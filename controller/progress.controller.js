a2d352f88ed729f4467202fd572993ac3c2ee161
Adith00SkyNet <adithe8401@gmail.com>
2026-01-02 11:58:03 +0530

    bug_fix

--- controller/progress.controller.js
+++ controller/progress.controller.js
@@ -3,14 +3,40 @@ import Chapter from "../models/chapter.model.js";
 import Lesson from "../models/lessons.model.js";
 
 export const initializeUserProgress = async (userId) => {
-  const firstChapter = await Chapter.findOne().sort({ order: 1 });
-  const firstLesson = await Lesson.findOne({ chapterId: firstChapter._id }).sort({ order: 1 });
+  if (!userId) {
+    throw new Error("initializeUserProgress: userId is required");
+  }
 
-  const progress = new UserProgress({
+  // Prevent duplicate progress creation
+  const existingProgress = await UserProgress.findOne({ userId });
+  if (existingProgress) {
+    return existingProgress;
+  }
+
+  // Try to find first chapter (may not exist yet)
+  const firstChapter = await Chapter.findOne().sort({ order: 1 });
+
+  // Try to find first lesson ONLY if chapter exists
+  let firstLesson = null;
+  if (firstChapter) {
+    firstLesson = await Lesson.findOne({
+      chapterId: firstChapter._id,
+    }).sort({ order: 1 });
+  }
+
+  // Create progress safely with fallbacks
+  const progress = await UserProgress.create({
     userId,
-    unlockedChapters: [firstChapter._id],
-    unlockedLessons: [firstLesson._id],
+    unlockedChapters: firstChapter ? [firstChapter._id] : [],
+    unlockedLessons: firstLesson ? [firstLesson._id] : [],
     completedLessons: [],
+    isInitialized: Boolean(firstChapter && firstLesson),
   });
-  await progress.save();
+
+  if (!progress?._id) {
+    throw new Error("initializeUserProgress: Progress creation failed");
+  }
+
+  return progress;
 };
+
